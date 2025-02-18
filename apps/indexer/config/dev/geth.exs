7e73b0215644fc8686add79fe3cd1a7ccc326a00
Meng Guang <mengguang@gmail.com>
2025-02-18 13:59:28 +0800

    change block_interval to 3s

--- apps/indexer/config/dev/geth.exs
+++ apps/indexer/config/dev/geth.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :indexer,
-  block_interval: :timer.seconds(5),
+  block_interval: :timer.seconds(3),
   json_rpc_named_arguments: [
     transport:
       if(System.get_env("ETHEREUM_JSONRPC_TRANSPORT", "http") == "http",
